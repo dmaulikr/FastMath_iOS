b05fed7e150fd76845774259de7be40fe1ee4131
jfrancop <juliofrp@gmail.com>
2016-06-27 22:23:42 -0700

    change ui, add shake when wrong, testing input for correct answer

--- FastMath/FastMathViewController.swift
+++ FastMath/FastMathViewController.swift
@@ -11,6 +11,17 @@ import Firebase
 
 class FastMathViewController: UIViewController {
     
+    @IBOutlet weak var firstOperandLabel: UILabel!
+    @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var secondOperandLabel: UILabel!
+    
+    @IBOutlet weak var resultTextView: UITextView! {
+        didSet {
+            resultTextView.delegate = self
+            resultTextView.layer.cornerRadius = 10.0
+        }
+    }
+    
     lazy var ref: FIRDatabaseReference = FIRDatabase.database().reference()
     var gameRef: FIRDatabaseReference!
     let mathProblemPath = "math"
@@ -20,6 +31,7 @@ class FastMathViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
         gameRef = ref.child(mathProblemPath)
         
         refHandle = gameRef.observeEventType(.Value, withBlock: { (snapshot) in
@@ -56,7 +68,40 @@ class FastMathViewController: UIViewController {
     }
     
     @IBAction func thridbutton(sender: AnyObject) {
+        resultTextView.shake()
     }
     @IBOutlet weak var thirdbttnlabel: UIButton!
 }
+
+extension FastMathViewController: UITextViewDelegate {
+    
+    func textViewDidChange(textView: UITextView) {
+        // check if answer is correct
+        
+        let length = textView.text.characters.count
+        if length == 3 {
+            // Only a max of three numbers is allowed
+            // Indicate answer is wrong by shaking textView and empty answer
+            
+            // shake textview duration 0.75, on completion clear out text
+            textView.text = ""
+        }
+        print("textview changed")
+    }
+
+}
+
+
+extension UIView {
+    // http://stackoverflow.com/questions/27987048/shake-animation-for-uitextfield-uiview-in-swift
+    func shake() {
+        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.duration = 0.4
+        animation.values = [-15.0, 15.0, -5.0, 5.0, 0.0 ]
+        layer.addAnimation(animation, forKey: "shake")
+    }
+}
+
+
 
